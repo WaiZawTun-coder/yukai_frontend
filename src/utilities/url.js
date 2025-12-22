bccec356ba8f8c48792d585f06372027baa938fe
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-22 18:08:14 +0630

    update: environments

--- src/utilities/url.js
+++ src/utilities/url.js
@@ -1,10 +1,6 @@
 const getBackendUrl = () => {
-  const environment = process.env.ENVIRONMENT;
-  console.log({ environments: process.env });
-  if (environment == "Production" || environment == "Preview") {
-    return "https://yukai-backend.onrender.com";
-  }
-  return "http://localhost/yukai_backend/public";
+  console.log({ url: process.env.NEXT_PUBLIC_BACKEND_URL });
+  return process.env.NEXT_PUBLIC_BACKEND_URL;
 };
 
 export { getBackendUrl };
