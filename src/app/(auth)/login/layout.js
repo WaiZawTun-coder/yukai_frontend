7bcd93b7c3f6c4787648779c38d58ee2068c40b2
WaiZawTun-coder <waizawtun.coder@gmail.com>
2025-12-24 10:57:59 +0630

    update: layout + sidebar

---
+++ src/app/(auth)/login/layout.js
@@ -0,0 +1,8 @@
+export const metadata = {
+  title: "Yukai - Login",
+  description: "Connect more firends",
+};
+
+export default function RootLayout({ children }) {
+  return <div>{children}</div>;
+}
