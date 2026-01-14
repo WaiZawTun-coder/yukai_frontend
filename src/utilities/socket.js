da3ec1b7a0109001b6d619b0c06b706f82545ecd
WaiZawTun-coder <waizawtun.coder@gmail.com>
2026-01-14 11:26:43 +0630

    update: socket testing

--- src/utilities/socket.js
+++ src/utilities/socket.js
@@ -2,6 +2,9 @@ import { io } from "socket.io-client";
 
 const socket = io("http://localhost:8080", {
   autoConnect: false,
+  reconnection: true,
+  reconnectionAttempts: 5,
+  transports: ["websocket"],
 });
 
 export { socket };
