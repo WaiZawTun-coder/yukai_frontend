81f5fcc1fc9fc780be2b5e01af2b0f7b9f8d47e2
WaiZawTun-coder <waizawtun.coder@gmail.com>
2026-02-17 21:20:45 +0630

    fix: icon color

--- src/components/PostCard/PostActions.jsx
+++ src/components/PostCard/PostActions.jsx
@@ -23,7 +23,7 @@ const reactionTypes = {
         width="28"
         height="28"
         viewBox="0 0 24 24"
-        fill="#f0f0f0"
+        fill="#3b82f6"
         xmlns="http://www.w3.org/2000/svg"
       >
         <path d="M1 21h4V9H1v12zM23 10c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 2 7.59 8.59C7.22 8.95 7 9.45 7 10v9c0 1.1.9 2 2 2h8c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1z" />
@@ -324,9 +324,19 @@ export default function PostActions({
           onTouchEnd={onTouchEnd}
         >
           <span className="reaction-main action-content">
-            {currentReaction
-              ? reactionTypes[currentReaction].icon
-              : reactionTypes.like.icon}
+            {currentReaction ? (
+              reactionTypes[currentReaction].icon
+            ) : (
+              <svg
+                width="28"
+                height="28"
+                viewBox="0 0 24 24"
+                fill="#f0f0f0"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M1 21h4V9H1v12zM23 10c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.44-1.06L14.17 2 7.59 8.59C7.22 8.95 7 9.45 7 10v9c0 1.1.9 2 2 2h8c.83 0 1.54-.5 1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1z" />
+              </svg>
+            )}
             {reactionCount}
           </span>
         </Button>
